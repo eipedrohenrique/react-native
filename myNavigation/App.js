a3f8b978e2ce6c934f65a2fc51fa447554dd9b9d
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-06-01 13:27:38 -0300

    navegação

--- myNavigation/App.js
+++ myNavigation/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigation } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Contacts from './src/pages/Contacts'
 import Information from './src/pages/Information'
 
-const Stack = createStackNavigation();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
--- myNavigation/src/pages/Contacts/index.js
+++ myNavigation/src/pages/Contacts/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Contacts() {
+export default function Contacts({navigation}) {
     return(
         <View>
-            <Text>Contacts</Text>
+            <Text>Pedro</Text>
+            <Text
+            onPress={() => navigation.navigate('Information')}
+            >Information...</Text>
         </View>
     )
 }
