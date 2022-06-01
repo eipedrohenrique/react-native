7d6e18d98672baef54d12abeb18f5a99f7cbd60a
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-06-01 15:02:05 -0300

    onebitcoin

---
+++ OneBitCoin/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, StatusBar, SafeAreaView, Platform } from 'react-native';
+
+import CurrentPrice from './src/components/CurrentPrice';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+      backgroundColor="#f50d41"
+      barStyle="light-content"
+      />
+      <CurrentPrice/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    paddingTop: Platform.OS === "android" ? 40: 0
+  },
+});
