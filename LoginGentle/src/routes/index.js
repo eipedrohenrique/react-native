bf99fbf93b64b6ba56430ba193698c2e348a1305
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-08-11 20:06:27 -0300

    telas

--- LoginGentle/src/routes/index.js
+++ LoginGentle/src/routes/index.js
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Welcome from '../pages/Welcome'
-import SignIn from '../pages/Signin'
+import SignIn from '../pages/SignIn'
 import Forgot from "../pages/Forgot";
 import Recovery from "../pages/Recovery";
 
